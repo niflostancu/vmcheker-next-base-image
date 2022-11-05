65622967ff544ac4318d330ed80f3f6d7cc4cf97
Mihai Baruta <baruta.mihai99@gmail.com>
2022-11-05 14:30:11 +0100

    Add fatal message if cd to checker directory is not possible

--- base/main.sh
+++ base/main.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+fatal () {
+    echo "$1" 1>&2
+    exit 1
+}
+
 rm -f /usr/local/bin/bash
 cp -rf /data/checker "$CI_PROJECT_DIR/"
 (
-    cd "$CI_PROJECT_DIR/checker" || exit 1
+    cd "$CI_PROJECT_DIR/checker" || fatal "The 'checker' directory was not found in your repository!"
     /bin/bash "./checker.sh"
 )
 exec /bin/bash "$@"
