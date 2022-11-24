89feff246678fcc4452211369a29ed5ef033b52b
Mihai Baruta <baruta.mihai99@gmail.com>
2022-11-24 23:58:36 +0100

    Add trace begin and end markers

--- base/main.sh
+++ base/main.sh
@@ -7,8 +7,12 @@ fatal () {
 
 rm -f /usr/local/bin/bash
 cp -rf /data/checker "$CI_PROJECT_DIR/"
+
+echo "<VMCK_NEXT_BEGIN>"    # begin trace mark
 (
     cd "$CI_PROJECT_DIR/checker" || fatal "The 'checker' directory was not found in your repository!"
     /bin/bash "./checker.sh"
 )
+echo "<VMCK_NEXT_END>"      # end trace mark
+
 exec /bin/bash "$@"
