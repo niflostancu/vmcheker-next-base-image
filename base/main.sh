55c1e859acf9643d7f2a01652bb69b8bb5cbbc69
Mihai Baruta <baruta.mihai99@gmail.com>
2024-11-13 23:25:01 +0200

    Remove .git && Run as an unprivileged user

--- base/main.sh
+++ base/main.sh
@@ -8,10 +8,17 @@ fatal () {
 rm -f /usr/local/bin/bash
 cp -rf /data/checker "$CI_PROJECT_DIR/"
 
+useradd -M -U mrchecker
+chown -R mrchecker:mrchecker "$CI_PROJECT_DIR"
+
+# We do not need the git repo anymore
+rm -rf "$CI_PROJECT_DIR/.git"
+
 echo "<VMCK_NEXT_BEGIN>"    # begin trace mark
 (
     cd "$CI_PROJECT_DIR/checker" || fatal "The 'checker' directory was not found in your repository!"
-    /bin/bash "./checker.sh"
+    unset "${!CI@}"
+    su -c '/bin/bash "./checker.sh"' mrchecker
 )
 echo "<VMCK_NEXT_END>"      # end trace mark
 
